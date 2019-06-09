2f7b3a93ab2883e0161993046dce323246232fda
League Student <league@iMac-7.attlocal.net>
2019-06-09 15:53:54 -0700

    StringIntegerPair, IntroToHasMaps, LogSearch

--- src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
+++ src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
@@ -5,12 +5,18 @@ import java.util.HashMap;
 public class _01_IntroToHashMaps {
 	public static void main(String[] args) {
 		//1. Create a HashMap called roots with Integers for the keys and Doubles for the values.
-
+			HashMap<Integer, Double> roots = new HashMap<Integer, Double>();
 		//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
 		//   current iteration of the loop (i). The value entry will be the square root of i.
+			for(int j = 0; j < 500; j++) {
+				roots.put(j, Math.sqrt(j));
+			}
 
 		//3. Iterate through all the entries in your HashMap displaying the keys with their respective
 		//   square roots (values). Use the following format.
+			for(int j = 0; j < roots.size(); j++) {
+				System.out.println("The square Root of "+j+" is "+roots.get(j));
+			}
 
 		//   The square Root of 0 is 0.0
 		//   The square Root of 1 is 1.0
--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,7 +1,15 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_LogSearch implements ActionListener {
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -29,5 +37,138 @@ public class _02_LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	
+	JFrame frame;
+	JPanel panel;
+	JButton addEntry;
+	JButton searchID;
+	JButton viewList;
+	JButton removeEntry;
+	HashMap<Long, String> information = new HashMap<Long, String>();
+	
+	public static void main(String[] args) {
+		_02_LogSearch ls = new _02_LogSearch();
+		ls.setup();
+	}
+	
+	void setup() {
+		frame = new JFrame();
+		panel = new JPanel();
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		addEntry = new JButton();
+		addEntry.setText("Add Entry");
+		addEntry.addActionListener(this);
+		panel.add(addEntry);
+		searchID = new JButton();
+		searchID.setText("Search by ID");
+		searchID.addActionListener(this);
+		panel.add(searchID);
+		viewList = new JButton();
+		viewList.setText("View List");
+		viewList.addActionListener(this);
+		panel.add(viewList);
+		removeEntry = new JButton();
+		removeEntry.setText("Remove Entry");
+		removeEntry.addActionListener(this);
+		panel.add(removeEntry);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton source = (JButton) e.getSource();
+		if(source == addEntry) {
+			enterEntry();
+		}
+		else if(source == searchID) {
+			searchInformation();
+		}
+		else if(source == viewList) {
+			printInformation();
+		}
+		else if(source == removeEntry) {
+			removeFromInformation();
+		}
+	}
+	
+	void enterEntry() {
+		String newIDStr = JOptionPane.showInputDialog("Enter the ID.");
+		String name = JOptionPane.showInputDialog("Enter the name.");
+		long newID;
+		try {
+			newID = Long.valueOf(newIDStr);
+			
+		}
+		catch(Exception e) {
+			JOptionPane.showMessageDialog(frame, "The ID couldn't be read as a number");
+			return;
+		}
+		if(information.containsKey(newID)) {
+			int continueAnswer = JOptionPane.showConfirmDialog(frame, "The ID already exists. Click Yes if you want to continue by changing the name of the existing ID.");
+			System.out.println(continueAnswer);
+			if(continueAnswer != 0) {
+				return;
+			}
+		}
+		information.put(newID, name);
+		JOptionPane.showMessageDialog(frame, "The ID "+newID+" has been set to the name "+name);
+	}
+	
+	void searchInformation() {
+		String searchStr = JOptionPane.showInputDialog("Enter the ID you want to search for.");
+		long idSearch;
+		try {
+			idSearch = Long.valueOf(searchStr);
+		}
+		catch(Exception e) {
+			JOptionPane.showMessageDialog(frame, "The ID couldn't be read");
+			return;
+		}
+		if(information.containsKey(idSearch)) {
+			JOptionPane.showMessageDialog(frame, "ID: "+idSearch+"\n"+"Name: "+information.get(idSearch));
+		}
+		else {
+			JOptionPane.showMessageDialog(frame, "The ID doesn't exist");
+			return;
+		}
+	}
+	
+	void printInformation() {
+		if(information.isEmpty() == true) {
+			JOptionPane.showMessageDialog(frame, "There are no entries.");
+			return;
+		}
+		String toPrint = "";
+		for(int j = 0; j < information.size(); j++) {
+			long anID = (long) information.keySet().toArray()[j];	
+			String aName = information.get(anID);
+			toPrint = toPrint + "ID: "+anID+" Name: "+aName+"\n";
+		}
+		JOptionPane.showMessageDialog(frame, toPrint);
+	}
+	
+	void removeFromInformation() {
+		String idStr = JOptionPane.showInputDialog("What is the ID of the name you want to remove?");
+		long removeID;
+		try {
+			removeID = Long.valueOf(idStr);
+		}
+		catch(Exception e) {
+			JOptionPane.showMessageDialog(frame, "The ID couldn't be read.");
+			return;
+		}
+		if(information.containsKey(removeID)) {
+			String name = information.get(removeID);
+			information.remove(removeID);
+			JOptionPane.showMessageDialog(frame, "The ID "+removeID+" (with the name of "+name+") has been removed.");
+		}
+		else {
+			JOptionPane.showMessageDialog(frame, "The ID doesn't exist.");
+		}
+	}
+	
 	
 }
